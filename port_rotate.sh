d480343120a0657b4140c7df73add7a68f4c45b2
Danilo Silva <danilocgsilva@gmail.com>
2021-07-03 23:21:51 -0300

    Ports rotate works on Mac OS

--- port_rotate.sh
+++ port_rotate.sh
@@ -4,12 +4,23 @@ set -e
 
 FILE_TARGET=.env
 
+if [ $(uname -s) = Darwin ]; then
+    if ! which gsed >> /dev/null; then
+        echo You are on a Mac, right? Sorry! You need first to install the gnu-sed utility via homebrew to make it work.
+        exit
+    else
+        SEDEXEC=gsed
+    fi
+else
+    SEDEXEC=sed
+fi
+
 replace() {
     OLD_PORT=$(sed -n $1p $FILE_TARGET | cut -f2 -d=)
     VARIABLE=$(sed -n $1p $FILE_TARGET | cut -f1 -d=)
     NEW_PORT=$(expr $OLD_PORT + 1)
 
-    sed -i $1s/$OLD_PORT/$NEW_PORT/g $FILE_TARGET
+    $SEDEXEC -i $1s/$OLD_PORT/$NEW_PORT/g $FILE_TARGET
     echo The variable $VARIABLE in $FILE_TARGET changed. From $OLD_PORT to $NEW_PORT
 }
 
